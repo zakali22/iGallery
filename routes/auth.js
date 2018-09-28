681cf43b119c3352d9d43653a165318446a9fccd
Zakeria Ali <zak.fined@gmail.com>
2018-09-28 18:10:38 +0100

    Setup the initial GooglePassport API

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+
+module.exports = app => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.get("/auth/google/callback", passport.authenticate("google"));
+};

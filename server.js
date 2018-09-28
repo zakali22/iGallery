e4b698b573f8e2a6f192f9bb538ce4801e9c196a
Zakeria Ali <zak.fined@gmail.com>
2018-09-28 14:49:22 +0100

    Create initial project structure

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cookieSession = require("cookie-session");

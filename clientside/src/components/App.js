a6107d6f947ec4ac28c1d1a516896f4acb71b123
Zakeria Ali <zak.fined@gmail.com>
2018-09-29 23:46:23 +0100

    Add homepage

--- clientside/src/components/App.js
+++ clientside/src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Homepage from "./Homepage/Homepage";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <React.Fragment>
+        <Homepage />
+      </React.Fragment>
+    );
   }
 }
 
---
+++ clientside/src/components/Homepage/Homepage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Homepage = props => {
+  return <div className="container" />;
+};
+
+export default Homepage;

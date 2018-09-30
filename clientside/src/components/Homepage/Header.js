aa4162895a60843f04cd468af42e4c047387497a
Zakeria Ali <zak.fined@gmail.com>
2018-09-30 11:53:15 +0100

    Create the Nav's Logo and Searchbar

---
+++ clientside/src/components/Homepage/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import SearchIcon from "../../img/search.svg";
+
+const Header = props => {
+  return (
+    <header className="header">
+      <a href="#">
+        <h3 className="header__logo">ig</h3>
+      </a>
+      <div className="header__searchContainer">
+        <input
+          type="text"
+          name="searchString"
+          className="header__searchContainer--input"
+          placeholder="Search..."
+        />
+        <img src={SearchIcon} className="header__searchContainer--searchIcon" />
+      </div>
+      <nav class="header__nav">
+        <div class="header__nav--item">
+          <span class="header__nav--text">Discover</span>
+        </div>
+        <div class="header__nav--item">
+          <span class="header__nav--text">Register/Login</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- clientside/src/components/Homepage/Homepage.js
+++ clientside/src/components/Homepage/Homepage.js
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Header from "./Header";
 
 const Homepage = props => {
-  return <div className="container" />;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 };
 
 export default Homepage;

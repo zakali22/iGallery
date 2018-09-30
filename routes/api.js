865865e5f3da43879e24d1e967259ccae897dbf9
Zakeria Ali <zak.fined@gmail.com>
2018-09-30 21:47:27 +0100

    Fix the Unsplash API routes

--- routes/api.js
+++ routes/api.js
@@ -7,14 +7,14 @@ const unsplash = require("../services/unsplash");
 const axios = require("axios");
 const queryString = require("query-string");
 
+const keys = require("../config/keys");
+
 module.exports = app => {
   // GET photos
   app.get("/api/unsplash/getPhotos", (req, res) => {
-    console.log(unsplash._bearerToken);
     axios({
       method: "GET",
-      url: "https://api.unsplash.com/photos?page=1&per_page=15&order_by=latest",
-      headers: { Authorization: "Bearer " + unsplash._bearerToken }
+      url: `https://api.unsplash.com/photos?client_id=${keys.unsplashKey}`
     }).then(response => {
       res.send(response.data);
     });
@@ -25,8 +25,9 @@ module.exports = app => {
     console.log(req.params.id);
     axios({
       method: "GET",
-      url: `https://api.unsplash.com/photos/${req.params.id}`,
-      headers: { Authorization: "Bearer " + unsplash._bearerToken }
+      url: `https://api.unsplash.com/photos/${req.params.id}?client_id=${
+        keys.unsplashKey
+      }`
     }).then(response => {
       res.send(response.data);
     });
@@ -38,8 +39,7 @@ module.exports = app => {
       method: "GET",
       url: `https://api.unsplash.com/search/photos?page=1&query=${
         req.body.searchString
-      }`,
-      headers: { Authorization: "Bearer " + unsplash._bearerToken }
+      }?client_id=${keys.unsplashKey}`
     }).then(response => {
       res.send(response.data);
     });
@@ -51,8 +51,7 @@ module.exports = app => {
       method: "GET",
       url: `https://api.unsplash.com/search/users?page=1&query=${
         req.body.searchString
-      }`,
-      headers: { Authorization: "Bearer " + unsplash._bearerToken }
+      }?client_id=${keys.unsplashKey}`
     }).then(response => {
       res.send(response.data);
     });
@@ -62,8 +61,9 @@ module.exports = app => {
   app.get("/api/unsplash/getProfile/:username", (req, res) => {
     axios({
       method: "GET",
-      url: `https://api.unsplash.com/users/${req.params.username}`,
-      headers: { Authorization: "Bearer " + unsplash._bearerToken }
+      url: `https://api.unsplash.com/users/${req.params.username}?client_id=${
+        keys.unsplashKey
+      }`
     }).then(response => {
       res.send(response.data);
     });
@@ -74,8 +74,9 @@ module.exports = app => {
     console.log(req.params.username);
     axios({
       method: "GET",
-      url: `https://api.unsplash.com/users/${req.params.username}/photos`,
-      headers: { Authorization: "Bearer " + unsplash._bearerToken }
+      url: `https://api.unsplash.com/users/${
+        req.params.username
+      }/photos?client_id=${keys.unsplashKey}`
     }).then(response => {
       res.send(response.data);
     });

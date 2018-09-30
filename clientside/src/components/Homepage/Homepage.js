54fd510ec2de701486636d106f80a58b441f8717
Zakeria Ali <zak.fined@gmail.com>
2018-09-30 18:33:41 +0100

    Re-organise the Jumbotron as Component

--- clientside/src/components/Homepage/Homepage.js
+++ clientside/src/components/Homepage/Homepage.js
@@ -1,24 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Header from "./Header";
-import ChevronIcon from "../../img/right-chevron.svg";
+import Jumbotron from "./Jumbotron";
 
 const Homepage = props => {
   return (
     <div className="container">
       <Header />
-      <div className="jumbotron-container">
-        <div className="jumbotron-container--text">
-          <h1>Find the moments you love best</h1>
-          <p>Discover world-class beautiful and timeless photos</p>
-        </div>
-        <div className="container__button">
-          <a href="#">
-            <span class="container__button--text">Explore photos</span>
-            <img src={ChevronIcon} className="container__button--icon" />
-          </a>
-        </div>
-      </div>
+      <Jumbotron />
     </div>
   );
 };
---
+++ clientside/src/components/Homepage/Jumbotron.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ChevronIcon from "../../img/right-chevron.svg";
+
+const Jumbotron = props => {
+  return (
+    <div className="jumbotron-container">
+      <div className="jumbotron-container--text">
+        <h1>Find the moments you love best</h1>
+        <p>Discover world-class beautiful and timeless photos</p>
+      </div>
+      <div className="container__button">
+        <a href="#">
+          <span class="container__button--text">Explore photos</span>
+          <img src={ChevronIcon} className="container__button--icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;

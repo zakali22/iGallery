e3fffb0ada1f67884d13042027e1272ea1ebf76d
Zakeria Ali <zak.fined@gmail.com>
2018-09-30 17:33:35 +0100

    Add jumbotron header

--- clientside/src/components/Homepage/Homepage.js
+++ clientside/src/components/Homepage/Homepage.js
@@ -1,11 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Header from "./Header";
+import ChevronIcon from "../../img/right-chevron.svg";
 
 const Homepage = props => {
   return (
     <div className="container">
       <Header />
+      <div className="jumbotron-container">
+        <div className="jumbotron-container--text">
+          <h1>Find the moments you love best</h1>
+          <p>Discover world-class beautiful and timeless photos</p>
+        </div>
+        <div className="container__button">
+          <a href="#">
+            <span class="container__button--text">Explore photos</span>
+            <img src={ChevronIcon} className="container__button--icon" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

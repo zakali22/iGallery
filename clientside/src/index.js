b3e7c4ff03cce75a04fe502dbf0daa3e67b00ca5
Zakeria Ali <zak.fined@gmail.com>
2018-10-01 18:22:02 +0100

    Persist the Redux store to the LocalStorage

--- clientside/src/index.js
+++ clientside/src/index.js
@@ -5,16 +5,26 @@ import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(
+  persistedReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
@@ -22,9 +32,13 @@ const store = createStore(
   )
 );
 
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );

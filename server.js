e09dd3574e003af13cb164482f8605500c2a35dd
Zakeria Ali <zak.fined@gmail.com>
2018-10-08 22:41:46 +0100

    Change the proxy

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const keys = require("./config/keys");
 // Header 'Allow-origin'
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://igallery-prod.herokuapp.com/"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
